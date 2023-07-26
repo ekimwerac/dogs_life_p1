2b1d1edb318d0ef9c4226297dbf77cbdef05d72e
Mike Carew <mike@carew.uk>
2023-07-26 23:14:50 +0100

    QL-6 Works and finished it off

--- superheroes/superheroes/src/main/java/com/example/superheroes/controller/HeroController.java
+++ superheroes/superheroes/src/main/java/com/example/superheroes/controller/HeroController.java
@@ -5,6 +5,7 @@ import com.example.superheroes.model.Hero;
 import com.example.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +18,10 @@ public class HeroController {
     @GetMapping("/heroes")
     public List<Hero> getAllHeroes(){
         return heroService.getAllHeroes();
+    }
+
+    @GetMapping("/heroes/{letter}")
+    public List<Hero> getHeroesStartingWithLetter(@PathVariable String letter){
+        return heroService.getHeroesStartingWithLetter(letter);
     }
 }
